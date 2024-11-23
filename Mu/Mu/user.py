b08bb500a082ac28bfb060f7e9b8524220bedbb0
steve14608 <plmnbvcxzaq0123456789@outlook.com>
2024-11-23 10:53:32 +0800

    ok

--- Mu/Mu/user.py
+++ Mu/Mu/user.py
@@ -1,8 +1,16 @@
 import json
 
 from django.http import HttpResponse
+from django.shortcuts import render
 import database
 import time
+
+
+def loginPage(request):
+    if request.COOKIES.get('user_id') is None:
+        return render(request, 'music/login1.html')
+    else:
+        return render(request, 'music/main.html')
 
 
 # 登录
@@ -11,7 +19,9 @@ def login(request):
     if user is not None:
         user_info = {'user_id': user.user_id, "user_bio": user.user_bio,
                      "user_nickname": user.user_nickname, "user_avatar": user.user_avatar}
-        return HttpResponse(json.dumps(user_info), status=200)
+        rep = HttpResponse(json.dumps(user_info), status=200)
+        rep.set_cookie('user_id', user.user_id)
+        return rep
     else:
         return HttpResponse(status=403)
     pass
@@ -24,23 +34,29 @@ def signup(request):
     else:
         ti = time.time()
         user = {'user_account': request['user_account'], 'user_password': request['password'], 'user_id': ti}
-        database.insert(request_name='user', val=request)
-        return HttpResponse(content=ti, status=200)
+        database.insert(request_name='user', val=user)
+        rep = HttpResponse(content=ti, status=200)
+        rep.set_cookie('user_id', ti)
+        return rep
 
 
 # 更新头图
 def updateAvatar(request):
+    user = database.query(request_name='user_id',val=request)
+    val = {'avatar_index':user.user_id,'avatar':request}
     pass
 
 
 # 更新签名
 def updateBio(request):
+    database.update('user', request)
+    return HttpResponse(status=200);
     pass
 
 
 # 获取信息
 def getUserDetail(request):
-    user = database.query(request_name='user_id')
+    user = database.query(request_name='user_id',val=request)
     user_info = {'user_id': user.user_id, "user_bio": user.user_bio,
                  "user_nickname": user.user_nickname, "user_avatar": user.user_avatar}
     return HttpResponse(json.dumps(user_info), status=200)
