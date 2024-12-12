a8f68d75f48f2396a8de167205f0133e3fc49a56
zzfx1 <2233214341@qq.com>
2024-12-12 13:02:24 +0800

    完成-1

--- Mu/static/js/saveAvatar.js
+++ Mu/static/js/saveAvatar.js
@@ -5,14 +5,19 @@ xhr.open('POST', '/getUserAvatar', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.setRequestHeader('X-CSRFToken',getCSRF());
 xhr.send();
-console.log(xhr.response);
+xhr.onreadystatechange=function(){
+    if(xhr.readyState==4 && xhr.status==200){
+        let src=xhr.responseURL;
+        console.log(src);
+        avatar.src=src;
+    }
+}
 function handleFiles(files) {
     if (files.length > 0) {
         let file = files[0]; 
         let reader = new FileReader();
         reader.onload = function(e) {
-            let imgElement = document.getElementById('avatar'); 
-            imgElement.src = e.target.result;
+            avatar.src = e.target.result;
         };
         reader.readAsDataURL(file); 
         const formData = new FormData();
